ee5e2333d79d11c773a657e8c08cfeeda267f8c9
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-02-11 11:33:19 +0100

    added refactoring to AddCarView

--- RentalCars/AddCarView.xaml.cs
+++ RentalCars/AddCarView.xaml.cs
@@ -38,35 +38,41 @@ namespace RentalCars
             }
             else
             {
-                try
-                {
-                    Car car = new Car()
-                    {
-                        Marka = TextBoxBrand.Text,
-                        Model = TextBoxModel.Text,
-                        RegistrationNumber = TextBoxRegistrationNumber.Text,
-                        Cost = Decimal.Parse(TextBoxCost.Text)
-                    };
-
-                    CarService carService = new CarService()
-                    {
-                        From = DateTime.Parse(TextBoxFrom.Text),
-                        To = DateTime.Parse(TextBoxFrom.Text).AddYears(1)
-                    };
-
-                    repository.AddCar(car, carService);
-                    ResetFields();
-                    MessageBox.Show("Nowy samochód został dodany");
-
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Nie udało się dodać samochodu", "", MessageBoxButton.OK,
-                        MessageBoxImage.Error);
-                }
+                SaveCarToDataBase();
             }
 
         }
+
+        private void SaveCarToDataBase()
+        {
+            try
+            {
+                Car car = new Car()
+                {
+                    Marka = TextBoxBrand.Text,
+                    Model = TextBoxModel.Text,
+                    RegistrationNumber = TextBoxRegistrationNumber.Text,
+                    Cost = Decimal.Parse(TextBoxCost.Text)
+                };
+
+                CarService carService = new CarService()
+                {
+                    From = DateTime.Parse(TextBoxFrom.Text),
+                    To = DateTime.Parse(TextBoxFrom.Text).AddYears(1)
+                };
+
+                repository.AddCar(car, carService);
+                ResetFields();
+                MessageBox.Show("Nowy samochód został dodany");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nie udało się dodać samochodu", "", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
 
         private string ValidateCar()
         {
