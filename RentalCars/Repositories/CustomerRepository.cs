9f3abe061e7ecf79830187f0dd93fff839d51372
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-02-11 16:06:19 +0100

    added CustomerRepository

---
+++ RentalCars/Repositories/CustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿using RentalCars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RentalCars.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly RentalCarDbContext db = null;
+
+        public CustomerRepository()
+        {
+            db = new RentalCarDbContext();
+        }
+
+        public Customer Get(int id)
+        {
+            return db.Customers.Find(id);
+        }
+
+        public List<Customer> GetAll()
+        {
+            return db.Customers.ToList();
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            if (customer != null)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
