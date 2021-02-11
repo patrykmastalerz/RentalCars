37fffa5e90f6996232bf7d74d117e7f0f01f7979
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-02-11 11:14:44 +0100

    added CarRepository

---
+++ RentalCars/Repositories/CarRepository.cs
@@ -0,0 +1,45 @@
+﻿using RentalCars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RentalCars.Repositories
+{
+    public class CarRepository
+    {
+
+        private readonly RentalCarDbContext db = null;
+
+        public CarRepository()
+        {
+            db = new RentalCarDbContext();
+        }
+
+        public Car Get(int id)
+        {
+            return db.Cars.Find(id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return db.Cars.ToList();
+        }
+
+        public void AddCar(Car car, CarService carService)
+        {
+            if (car != null && carService != null)
+            {
+                car.CarService = carService;
+                db.Cars.Add(car);
+                db.SaveChanges();
+
+                db.SaveChanges();
+
+            }
+        }
+
+       
+
+    }
+}
