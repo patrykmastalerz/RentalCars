d3dc83f9bc2386909a4a6b2b991ef57acd570749
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-02-11 11:02:43 +0100

    added AddCarView and styles

--- RentalCars/MainWindow.xaml.cs
+++ RentalCars/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentalCars.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace RentalCars
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void addCarBatton_Click(object sender, RoutedEventArgs e)
+        {
+            AddCarView carView = new AddCarView();
+            carView.Show();
         }
     }
 }
