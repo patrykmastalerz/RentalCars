1419a96767cb52e09874aff5131852fad35dcc7f
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-02-11 16:17:55 +0100

    added validation to addCustomerView and ResetFields

--- RentalCars/AddCustomerView.xaml.cs
+++ RentalCars/AddCustomerView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentalCars.Models;
+using RentalCars.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +19,80 @@ namespace RentalCars
     /// </summary>
     public partial class AddCustomerView : Window
     {
+        private readonly CustomerRepository repository = null;
         public AddCustomerView()
         {
             InitializeComponent();
+            repository = new CustomerRepository();
         }
+
+        private void addCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var validationMessage = ValidateCustomer();
+            if (string.IsNullOrEmpty(validationMessage) == false)
+            {
+                MessageBox.Show(validationMessage);
+            }
+            else
+            {
+                SaveCustomerToDataBase();
+            }
+        }
+
+        private void SaveCustomerToDataBase()
+        {
+            try
+            {
+                Customer customer = new Customer()
+                {
+                    FirstName = TextBoxFirstName.Text,
+                    SecondName = TextBoxSecondName.Text,
+                    PhoneNumber = TextBoxPhoneNumber.Text
+                };
+
+                repository.AddCustomer(customer);
+                ResetFields();
+                MessageBox.Show("Nowy użytkownik został dodany");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nie udało się dodać użytkownika", "", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private string ValidateCustomer()
+        {
+            string output = "";
+            if (string.IsNullOrEmpty(TextBoxFirstName.Text))
+            {
+                output = "Wprowadź Imie!";
+            }
+            else if (TextBoxFirstName.Text.Length >= 30)
+            {
+                output = "Przekroczono liczbe znaków!";
+            }
+            else if (string.IsNullOrEmpty(TextBoxSecondName.Text))
+            {
+                output = "Wprowadź Nazwisko!";
+            }
+            else if (TextBoxSecondName.Text.Length >= 30)
+            {
+                output = "Przekroczono liczbe znaków!";
+            }
+            else if ((TextBoxPhoneNumber.Text.Length != 9) || System.Text.RegularExpressions.Regex.IsMatch(TextBoxPhoneNumber.Text, "[^0-9]"))
+            {
+                output = "Wprowadzono nie prawidłowy numer telefonu!";
+            }
+            return output;
+        }
+
+        private void ResetFields()
+        {
+            TextBoxFirstName.Text = "";
+            TextBoxSecondName.Text = "";
+            TextBoxPhoneNumber.Text = "";
+        }
+
     }
 }
