424170aa9dfb2d957c3bee35dad6122d08f31cd4
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-02-17 15:22:21 +0100

    added documentation to CarService

--- RentalCars/Models/CarService.cs
+++ RentalCars/Models/CarService.cs
@@ -6,16 +6,36 @@ using System.Text;
 
 namespace RentalCars.Models
 {
+    /// <summary>
+    /// Model CarService opisuje przegląd techniczny samochodu
+    /// </summary>
     public class CarService
     {
+        /// <summary>
+        /// Klucz głowny modelu Car
+        /// </summary>
         [Key]
         public int CarServiceId { get; set; }
+
+        /// <summary>
+        /// Pole From określa czas od którego ważny jest przegląd techniczny
+        /// </summary>
         public DateTime From { get; set; }
+
+        /// <summary>
+        /// Pole To określa czas, do którego ważny przegląd techniczny
+        /// </summary>
         public DateTime To { get; set; }
 
+        /// <summary>
+        /// Klucz obcy modelu Car
+        /// </summary>
         [ForeignKey("Car")]
         public int CarId { get; set; }
 
+        /// <summary>
+        /// Zależność do klucza obcego Car
+        /// </summary>
         public virtual Car Car { get; set; }
     }
 }
